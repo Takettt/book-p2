4004f3fadc8aaf569eb2b9ee31df703f4ab6f36b
Takettt <dashuzhuyuan@gmail.com>
2024-02-04 14:11:57 +0000

    book update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,24 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def create
+    @book = current_user.books.build(book_params)
+    if @book.save
+      redirect_to request.referer, notice: "You have created book successfully."
+    else
+      @user = current_user
+      @books = Book.all
+      render "index"
+      @book = Book.new
+    end
   end
 
   def index
+    @user = current_user
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -18,5 +31,11 @@ class BooksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :option, :profile_image)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,27 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @book = Book.new
+    @books = @user.books
   end
 
   def index
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "You have updated user successfully."
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:profile_image)
   end
 end
